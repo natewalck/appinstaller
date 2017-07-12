b0603a36082131df768261a1c448dffa382d4b38
Nate Walck <nate.walck@gmail.com>
2017-07-11 20:34:53 -0400

    Updated to use new version of macosutils

--- appinstaller.go
+++ appinstaller.go
@@ -3,34 +3,35 @@
 package main
 
 import (
-    "flag"
-    "github.com/natewalck/macosutils"
-    "log"
+	"flag"
+	"log"
+
+	"github.com/natewalck/macosutils"
 )
 
 func main() {
-    var (
-        dmgPath = flag.String("dmg", "", "path to dmg")
-    )
-    flag.Parse()
+	var (
+		dmgPath = flag.String("dmg", "", "path to dmg")
+	)
+	flag.Parse()
 
-    my_dmg := macosutils.DMG{}
-    my_dmg.Mount(*dmgPath)
-    my_dmg.GetInstallables()
-    if len(my_dmg.Pkgs) > 0 {
-        for _, p := range my_dmg.Pkgs {
-            log.Printf("Installing: %v\n", p)
-            macosutils.InstallPkg(my_dmg.MountPoint, p)
-        }
-    } else if len(my_dmg.Apps) > 0 {
-        for _, a := range my_dmg.Apps {
-            log.Printf("Installing: %v\n", a)
-            macosutils.InstallApp(my_dmg.MountPoint, a)
-        }
-    } else {
-        log.Print("Nothing found to install.")
-    }
+	myDmg := macosutils.NewDMG(*dmgPath)
+	myDmg.Mount()
+	myDmg.GetInstallables()
+	if len(myDmg.Pkgs) > 0 {
+		for _, p := range myDmg.Pkgs {
+			log.Printf("Installing: %v\n", p)
+			macosutils.InstallPkg(myDmg.MountPoint, p)
+		}
+	} else if len(myDmg.Apps) > 0 {
+		for _, a := range myDmg.Apps {
+			log.Printf("Installing: %v\n", a)
+			macosutils.InstallApp(myDmg.MountPoint, a)
+		}
+	} else {
+		log.Print("Nothing found to install.")
+	}
 
-    my_dmg.Unmount(my_dmg.MountPoint)
-    log.Printf("Unmounted dmg...")
+	myDmg.Unmount(myDmg.MountPoint)
+	log.Printf("Unmounted dmg...")
 }
